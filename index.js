50a7e3b09a062bfa241c34c6b09cb69d14060229
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2022-06-19 11:58:39 +0700

    Add moment locale

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const moment = require("moment")
 
 const displayTime = () => {
+    moment.locale('id');
+
     $('.time').text(moment().format('LTS'));
     $('.date').text(moment().format('LL'));
 };
